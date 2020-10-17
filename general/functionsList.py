aaeb893c31a121830c6166862a13d08e395a635e
Geulmaster <eyal.geulayev@gmail.com>
2020-10-17 23:09:40 +0300

    Add name_manipulator and dir_info functions to basic.py

--- general/functionsList.py
+++ general/functionsList.py
@@ -1,2 +1,2 @@
-basic_functions_list = ["create_file", "copy_file", "copy_folder","copy_multiple", "move", "delete_file", "delete_folder", "delete_multiple", "Exit"]
+basic_functions_list = ["create_file", "copy_file", "copy_folder","copy_multiple", "move", "delete_file", "delete_folder", "delete_multiple", "dir_info", "name_manipulator", "Exit"]
 hardware_functions_list=["CPU", "RAM", "Memory", "processes", "network", "Exit"]
--- methods/basic.py
+++ methods/basic.py
@@ -155,6 +155,29 @@ class Basic():
         else:
             logger.info("Ok, {} is an empty file for now".format(file_name))
 
+    def name_manipulator():
+        """
+        Replaces parts of names
+        example: name_manipulator("serhio", "kopo")
+        """
+        source = input("Insert a directory: ")
+        old_str = input("Enter the sequence you would like to change: ")
+        new_str = input("Enter the sequence you would like to set instead: ")
+
+        for root, dirs, files in os.walk(source):
+            for name in files:
+                new_name = name.replace(old_str, new_str)
+                os.rename(os.path.join(root, name), os.path.join(root, new_name))
+                print(f"{name} changed to {new_name}")
+
+    def dir_info():
+        """
+        Returns number of files in directory
+        """
+        source = input("Insert a directory: ")
+        number_of_files = len([files for root, dirs, files in os.walk(source) for name in files if os.path.isfile(os.path.join(root, name))])
+        print(f"There are {number_of_files} files in {source}")
+        return number_of_files
 
     def exit(self):
         logger.info("Bye!")
