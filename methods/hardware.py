a38b20a362ebc774d80926465c68cb278dc0b610
Geulmaster <eyal.geulayev@gmail.com>
2021-02-06 17:26:13 +0200

    Add GPU info

--- methods/hardware.py
+++ methods/hardware.py
@@ -2,6 +2,8 @@ import sys
 import memory_profiler as memP
 import psutil
 import speedtest
+import GPUtil
+from tabulate import tabulate
 
 def memory():
     for value in memP.memory_usage():
@@ -30,12 +32,29 @@ def processes():
             pass
 
 def network():
+    print("Please wait a few seconds...")
     st = speedtest.Speedtest()
     st.get_best_server()
     download_speed = st.download() * 0.000001 #Converts bytes to MBs
     upload_speed = st.upload() * 0.000001
     print(f"""Download speed is {download_speed} MB per second
 Upload speed is {upload_speed} MB per second""")
+
+def graphics():
+    gpus = GPUtil.getGPUs()
+    gpus_list = []
+    for gpu in gpus:
+        gpu_id = gpu.id
+        gpu_name = gpu.name
+        gpu_load = f"{gpu.load*100}%"
+        gpu_free_memory = f"{gpu.memoryFree}MB"
+        gpu_used_memory = f"{gpu.memoryUsed}MB"
+        gpu_total_memory = f"{gpu.memoryTotal}MB"
+        gpu_temperature = f"{gpu.temperature} C"
+        gpus_list.append((gpu_id, gpu_name, gpu_load, 
+        gpu_free_memory, gpu_used_memory, gpu_total_memory, gpu_temperature))
+    print(str(tabulate(gpus_list, headers=("id", "name", "load",
+    "free memory", "used memory", "total memory", "temperature"))), "\n")
 
 def exit():
     sys.exit()
