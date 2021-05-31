f340b4640b7baef4366ef3dfd688bec50ecd9952
Geulmaster <eyal.geulayev@gmail.com>
2021-06-01 00:01:50 +0300

    Add time and date functions

--- serjio/main.py
+++ serjio/main.py
@@ -1,13 +1,30 @@
 import pyttsx3
+from datetime import datetime
+from time import time
 
 class Serjio:
 
     def __init__(self, text):
-        engine = pyttsx3.init()
-        engine.say(text)
-        engine.runAndWait()
+        self.engine = pyttsx3.init()
+        self.speak("Hello Sir")
+
+    def speak(self, audio):
+        self.engine.say(audio)
+        self.engine.runAndWait()
+
+    def time(self):
+        current_time = datetime.now().strftime("%H:%M:%S")
+        self.speak(f"It is {current_time}")
+
+    def date(self):
+        timestamp = int(time())
+        date = datetime.fromtimestamp(timestamp)
+        current_date = date.strftime("%B,%d,%Y")
+        self.speak(f"It is {current_date}")
+
 
 """
 Example:
-run = Serjio("Yo yo")
+run = Serjio("Hello Sir")
+run.date()
 """
